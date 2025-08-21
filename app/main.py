70983257e9430bf0d906acb43a90c8c0a297eedb
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-08-21 13:16:38 +0300

    feat: configure logging settings

--- app/main.py
+++ app/main.py
@@ -7,6 +7,11 @@ app = FastAPI()
 manager = Manager()
 logger = logging.getLogger(__name__)
 
+base_config = logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        handlers=logging.StreamHandler())
+
 
 @app.get("/process_data")
 def process_data():
