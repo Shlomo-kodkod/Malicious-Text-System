0e48b447b37369471db61baa1fdd12c5ccb401c5
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-08-21 10:18:11 +0300

    feat: implement database connection and data retrieval in DAL class

---
+++ app/fetcher.py
@@ -0,0 +1,56 @@
+from pymongo import MongoClient
+import pandas as pd 
+import logging
+import config  
+
+
+logger = logging.getLogger(__name__)
+
+
+class DAL:
+    def __init__(self):
+        self.__client = None
+        self.__db = None
+        self.__df = None
+
+
+    def connect(self):
+        """
+        Connect to the mongo atlas database.
+        """
+        try:
+            uri = f"mongodb+srv://{config.username}:{config.password}@{config.db}.gurutam.mongodb.net/"
+            self.__client = MongoClient(uri)
+            self.__db = self.__client[config.db]
+            logger.info(f"Connected to mongodb.")
+        except Exception as e:
+            logger.error(f"Failed to connect to mongodb: {e}")
+            raise e
+
+    def disconnect(self):
+        """
+        Disconnect from the mongo atlas database.
+        """ 
+        if self.__client is not None:
+            self.__client.close()
+            logger.info("Database connection closed.")
+
+
+    def read_collection(self, collection_name=config.collection):
+        """""
+        Read a collection from the mongo atlas database.
+        """
+        try:
+            collection = self.__db[collection_name]
+            data = list(collection.find({}, {"_id": 0}))  
+            logger.info(f"Data loaded successfully.")
+            self.__df = pd.DataFrame(data)
+        except Exception as e:
+            logger.error(f"Failed to retrieve collection: {e}")
+            raise e
+        
+    
+    @property
+    def get_df(self):
+        return self.__df
+
