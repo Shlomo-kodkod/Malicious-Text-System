1046d1b0f1cbbb08894fd4259abfeb24bfad6649
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-08-21 13:10:47 +0300

    feat: implement Manager class for data loading and processing with sentiment analysis and weapons detection

--- app/config.py
+++ app/config.py
@@ -6,4 +6,4 @@ password=os.getenv("MONGO_INITDB_ROOT_PASSWORD", "iraniraniran")
 db = os.getenv("MONGO_INITDB_DATABASE", "IranMalDB")
 collection = os.getenv("MONGO_COLLECTION", "tweets")
 
-blacklist_path = r"C:\Users\user\Projects\Python\Malicious Text System\data\weapon_list.txt"
+blacklist_path = r"data/weapon_list.txt"
