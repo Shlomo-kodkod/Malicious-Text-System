f518e0040c09ea6051e58b93ea63227bf36a07f7
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-08-21 18:58:27 +0300

    edit route

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -7,7 +7,5 @@ cd infra
 oc apply -f secrets.yaml
 oc apply -f deployment.yaml
 oc apply -f service.yaml
+oc apply -f route.yaml
 
-oc expose service  malicious-text-system --port=8085 --target-port=8085 --name=malicious-text-system
-
-oc annotate  route malicious-text-system haproxy.router.openshift.io/timeout=2m --overwrite
